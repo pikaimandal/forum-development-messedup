3061a5630594d61af64f2fce7bdd545b79e32005
Pikai Mandal <pikaimandal@hotmail.com>
2025-09-06 19:01:47 +0530

    add real worldcoin logo

--- components/login-screen.tsx
+++ components/login-screen.tsx
@@ -32,14 +32,11 @@ export function LoginScreen({ onLogin }: LoginScreenProps) {
           </div>
 
           <Button onClick={onLogin} className="w-full h-12 text-lg font-semibold" size="lg">
-            <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
+            <img 
+              src="/worldcoinlogo.png" 
+              alt="Worldcoin Logo" 
+              className="w-5 h-5 mr-2 object-contain"
+            />
             Login with Wallet
           </Button>
 
